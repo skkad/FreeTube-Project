6e8889c2ab88558ca6f9bd90a0c2652bd68984a2
skkad <skkad1998@gmail.com>
2022-10-12 22:20:23 +0530

    video channels components commit

--- src/Components/ChannelDetails.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ChannelDetails(){
-    return(
-        <>
-            <div>ChannelDetails</div>
-        </>
-    )
-}
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,8 +1,11 @@
 export {default as Navbar} from "./Navbar";
 export {default as Feed} from "./Feed";
 export {default as VideoDetails} from "./VideoDetails";
-export {default as ChannelDetails} from "./ChannelDetails";
+// export {default as ChannelDetails} from "./ChannelDetails";
 export {default as SearchFeed} from "./SearchFeed";
 export {default as SearchBar} from "./SearchBar";
 export {default as SideBar} from "./SideBar";
 export {default as Videos} from "./Videos";
+export { default as Loader } from './Loader';
+export { default as ChannelDetail } from './ChannelDetail';
+export { default as ChannelCard } from './ChannelCard';
