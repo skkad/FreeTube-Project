0d317356771c138fcf10403f14ffb11f99d9c8ae
skkad <skkad1998@gmail.com>
2022-10-17 16:32:13 +0530

    final working commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter ,Routes,Route } from "react-router-dom";
 import { Box } from "@mui/material";
-import {Navbar,Feed ,VideoDetails,ChannelDetails,SearchFeed} from './Components/index'
+import {Navbar,Feed ,VideoDetails,ChannelDetail,SearchFeed} from './Components/index'
 
 export default function App(){
     return(
@@ -11,7 +11,7 @@ export default function App(){
                 <Routes>
                     <Route path="/" element={<Feed/>} />
                     <Route path="/video/:id" element={<VideoDetails/>} />
-                    <Route path="/channel/:id" element={<ChannelDetails/>} />
+                    <Route path="/channel/:id" element={<ChannelDetail/>} />
                     <Route path="/search/:searchTerm" element={<SearchFeed/>} />
                 </Routes>
             </Box>
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,7 +1,7 @@
 export {default as Navbar} from "./Navbar";
 export {default as Feed} from "./Feed";
 export {default as VideoDetails} from "./VideoDetails";
-// export {default as ChannelDetails} from "./ChannelDetails";
+export {default as VideoCard} from "./VideoCard";
 export {default as SearchFeed} from "./SearchFeed";
 export {default as SearchBar} from "./SearchBar";
 export {default as SideBar} from "./SideBar";
